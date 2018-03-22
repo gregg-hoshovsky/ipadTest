5cfe40751fb46317aefde44388e1f9c5fa8d4d48
Gregg <gregg.hoshovsky@centurylink.net>
2018-03-21 18:38:03 -0700

    first commit

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,191 @@
+// (c) 2014 Don Coleman
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* global mainPage, deviceList, refreshButton */
+/* global detailPage, resultDiv, messageInput, sendButton, disconnectButton */
+/* global ble  */
+/* jshint browser: true , devel: true*/
+'use strict';
+
+// ASCII only
+function bytesToString(buffer) {
+    return String.fromCharCode.apply(null, new Uint8Array(buffer));
+}
+
+// ASCII only
+function stringToBytes(string) {
+    var array = new Uint8Array(string.length);
+    for (var i = 0, l = string.length; i < l; i++) {
+        array[i] = string.charCodeAt(i);
+    }
+    return array.buffer;
+}
+
+// this is Nordic's UART service
+var bluefruit = {
+    serviceUUID: '6e400001-b5a3-f393-e0a9-e50e24dcca9e',
+    txCharacteristic: '6e400002-b5a3-f393-e0a9-e50e24dcca9e', // transmit is from the phone's perspective
+    rxCharacteristic: '6e400003-b5a3-f393-e0a9-e50e24dcca9e'  // receive is from the phone's perspective
+};
+
+var app = {
+    initialize: function() {
+        this.bindEvents();
+        detailPage.hidden = true;
+    },
+    bindEvents: function() {
+        console.log("BluefruitLE: bindEvents");
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        refreshButton.addEventListener('touchstart', this.refreshDeviceList, false);
+        sendButton.addEventListener('click', this.sendData, false);
+        disconnectButton.addEventListener('touchstart', this.disconnect, false);
+        deviceList.addEventListener('touchstart', this.connect, false); // assume not scrolling
+    },
+    onDeviceReady: function() {
+      console.log("BluefruitLE: onDeviceReady");
+        app.refreshDeviceList();
+    },
+    refreshDeviceList: function() {
+      console.log("BluefruitLE: refreshDeviceList");
+        deviceList.innerHTML = ''; // empties the list
+      //  ble.scan([], 5, app.onDiscoverDevice, app.onError);
+      console.log("BluefruitLE: DONE refreshDeviceList");
+        // if Android can't find your device try scanning for all devices
+        // ble.scan([], 5, app.onDiscoverDevice, app.onError);
+        ble.startScan([], function(device) {
+            console.log("BluefruitLE:"+JSON.stringify(device));
+        }, failure);
+
+        setTimeout(ble.stopScan,
+            50000,
+            function() { console.log("BluefruitLE:"+"Scan complete"); },
+            function() { console.log("BluefruitLE:"+"stopScan failed"); }
+        );
+    },
+    onDiscoverDevice: function(device) {
+      console.log("BluefruitLE- onDiscoverDevice: "+device);
+
+      var SERVICE_DATA_KEY = '0x16';
+      var advertisingData = parseAdvertisingData(device.advertising);
+      var serviceData = advertisingData[SERVICE_DATA_KEY];
+          console.log("BluefruitLE- serviceData: "+deserviceDatavice);
+      if (serviceData) {
+          // first 2 bytes are the 16 bit UUID
+          var uuidBytes = new Uint16Array(serviceData.slice(0,2));
+          var uuid = uuidBytes[0].toString(16); // hex string
+          console.log("Found service data for " + uuid); // should be 0xFEAA
+
+          // remaining bytes are the Eddystone beacon data
+          var eddystoneData = new Uint8Array(serviceData.slice(2));
+            console.log("BluefruitLE- eddystoneData: "+ eddystoneData);
+          // do something with the eddystoneData
+      }
+      console.log("BluefruitLE: "+ JSON.stringify(device));
+        console.log("BluefruitLE: onDiscoverDevice");
+        var listItem = document.createElement('li'),
+            html = '<b>' + device.name + '</b><br/>' +
+                'RSSI: ' + device.rssi + '&nbsp;|&nbsp;' +
+                device.id;
+
+        listItem.dataset.deviceId = device.id;
+        listItem.innerHTML = html;
+        deviceList.appendChild(listItem);
+    },
+    connect: function(e) {
+            console.log("BluefruitLE: connect");
+        var deviceId = e.target.dataset.deviceId,
+            onConnect = function(peripheral) {
+                app.determineWriteType(peripheral);
+
+                // subscribe for incoming data
+                ble.startNotification(deviceId, bluefruit.serviceUUID, bluefruit.rxCharacteristic, app.onData, app.onError);
+                sendButton.dataset.deviceId = deviceId;
+                disconnectButton.dataset.deviceId = deviceId;
+                resultDiv.innerHTML = "";
+                app.showDetailPage();
+            };
+
+        ble.connect(deviceId, onConnect, app.onError);
+    },
+    determineWriteType: function(peripheral) {
+        // Adafruit nRF8001 breakout uses WriteWithoutResponse for the TX characteristic
+        // Newer Bluefruit devices use Write Request for the TX characteristic
+        console.log("BluefruitLE: determineWriteType");
+        var characteristic = peripheral.characteristics.filter(function(element) {
+            if (element.characteristic.toLowerCase() === bluefruit.txCharacteristic) {
+                return element;
+            }
+        })[0];
+
+        if (characteristic.properties.indexOf('WriteWithoutResponse') > -1) {
+            app.writeWithoutResponse = true;
+        } else {
+            app.writeWithoutResponse = false;
+        }
+
+    },
+    onData: function(data) { // data received from Arduino
+          console.log("BluefruitLE: onData");
+        console.log(data);
+        resultDiv.innerHTML = resultDiv.innerHTML + "Received: " + bytesToString(data) + "<br/>";
+        resultDiv.scrollTop = resultDiv.scrollHeight;
+    },
+    sendData: function(event) { // send data to Arduino
+          console.log("BluefruitLE: sendData");
+        var success = function() {
+            console.log("success");
+            resultDiv.innerHTML = resultDiv.innerHTML + "Sent: " + messageInput.value + "<br/>";
+            resultDiv.scrollTop = resultDiv.scrollHeight;
+        };
+
+        var failure = function() {
+            alert("Failed writing data to the bluefruit le");
+        };
+
+        var data = stringToBytes(messageInput.value);
+        var deviceId = event.target.dataset.deviceId;
+
+        if (app.writeWithoutResponse) {
+            ble.writeWithoutResponse(
+                deviceId,
+                bluefruit.serviceUUID,
+                bluefruit.txCharacteristic,
+                data, success, failure
+            );
+        } else {
+            ble.write(
+                deviceId,
+                bluefruit.serviceUUID,
+                bluefruit.txCharacteristic,
+                data, success, failure
+            );
+        }
+
+    },
+    disconnect: function(event) {
+        var deviceId = event.target.dataset.deviceId;
+        ble.disconnect(deviceId, app.showMainPage, app.onError);
+    },
+    showMainPage: function() {
+        mainPage.hidden = false;
+        detailPage.hidden = true;
+    },
+    showDetailPage: function() {
+        mainPage.hidden = true;
+        detailPage.hidden = false;
+    },
+    onError: function(reason) {
+        alert("ERROR: " + JSON.stringify(reason)); // real apps should use notification.alert
+    }
+};
