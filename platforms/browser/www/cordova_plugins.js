0b919ecb62cb452c7187583f6ba7f470378f507c
Gregg <gregg.hoshovsky@centurylink.net>
2018-03-21 19:27:33 -0700

    update

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,27 +1,20 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-ble-central/www/ble.js",
-        "id": "cordova-plugin-ble-central.ble",
-        "pluginId": "cordova-plugin-ble-central",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
         "clobbers": [
-            "ble"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-ble-central/src/browser/BLECentralPlugin.js",
-        "id": "cordova-plugin-ble-central.BLECentralPlugin",
-        "pluginId": "cordova-plugin-ble-central",
-        "merges": [
-            "ble"
+            "window.StatusBar"
         ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-compat": "1.2.0",
-    "cordova-plugin-ble-central": "1.1.4"
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-console": "1.0.7",
+    "cordova-plugin-statusbar": "1.0.1"
 }
 // BOTTOM OF METADATA
 });
